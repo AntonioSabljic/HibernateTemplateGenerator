193fe3b16377c090699dcf4536434e5348efb91d
GitHub Action <action@github.com>
2021-12-30 09:38:12 +0000

    Template cleanup

--- src/main/kotlin/com/github/antoniosabljic/hibernatetemplategenerator/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/antoniosabljic/hibernatetemplategenerator/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.antoniosabljic.hibernatetemplategenerator.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.antoniosabljic.hibernatetemplategenerator.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
---
+++ src/main/kotlin/com/github/antoniosabljic/hibernatetemplategenerator/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.antoniosabljic.hibernatetemplategenerator.services
+
+import com.github.antoniosabljic.hibernatetemplategenerator.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
